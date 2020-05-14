bbfd36429501ca932dc04cd675ee8de0f001ba6b
dem <landrok@users.noreply.github.com>
2020-05-14 21:24:54 +0200

    Refactor BasicProcess class

---
+++ src/Yousign/Process/AbstractProcess.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Yousign\Process;
+
+use Exception;
+use Yousign\Model\Factory;
+use Yousign\Model\FileCollection;
+use Yousign\Model\Procedure;
+use Yousign\YousignApi;
+
+/*
+ * Basic class for a process
+ */
+abstract class AbstractProcess
+{
+    /**
+     * Yousign API wrapper
+     *
+     * @var \Yousign\YousignApi
+     */
+    protected $api;
+
+    /**
+     * Files to send
+     *
+     * @var \Yousign\Model\FileCollection
+     */
+    protected $files;
+
+    /**
+     * Procedure to send
+     *
+     * @var \Yousign\Model\Procedure
+     */
+    protected $procedure;
+
+    public function __construct(YousignApi $api)
+    {
+        $this->api = $api;
+        $this->files = Factory::createFileCollection();
+        $this->procedure = Factory::createProcedure([]);
+    }
+
+    /**
+     * Add one file to the process
+     *
+     * @return self
+     */
+    public function addFile(array $file): self
+    {
+        $this->files->add(
+            Factory::createFile($file)
+        );
+
+        return $this;
+    }
+
+    /**
+     * Add the procedure to the process
+     *
+     * @return self
+     */
+    public function setProcedure(array $procedure): self
+    {
+        $this->procedure = Factory::createProcedure($procedure);
+
+        return $this;
+    }
+
+    /**
+     * Get the initial procedure before calling the API
+     *  or the final result when API has been called with success
+     *
+     * @return \Yousign\Model\Procedure
+     */
+    public function getProcedure(): Procedure
+    {
+        return $this->procedure;
+    }
+
+    /**
+     * Get the initial files before calling the API
+     *  or the final results when API has been called with success
+     *
+     * @return \Yousign\Model\FileCollection
+     */
+    public function getFiles(): FileCollection
+    {
+        return $this->files;
+    }
+}
